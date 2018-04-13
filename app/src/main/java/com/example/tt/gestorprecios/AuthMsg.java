bc47f36ca410f971df8c560203bd6eb45541c46f
CarlosRicagno <cr.ricagno@gmail.com>
2018-04-13 02:28:27 -0500

    Version compilando, faltan:imagenes pngs acordes

---
+++ app/src/main/java/com/example/tt/gestorprecios/AuthMsg.java
@@ -0,0 +1,21 @@
+package com.example.tt.gestorprecios;
+
+
+public class AuthMsg {
+    private float precio;
+    private String nombre;
+
+    public float getPrecio() {
+        return precio;
+    }
+    public void setCode(float precio) {
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setMessage(String message) {
+        this.nombre = message;
+    }
+}
